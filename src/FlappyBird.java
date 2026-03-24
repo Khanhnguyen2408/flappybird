7da19b6c744e8749c5e3d870d45a20668c9f5194
khanh <nguyenquocckhanhnd@gmail.com>
2026-03-24 16:46:16 +0700

    them man hinh high score

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,7 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 import javax.swing.*;
 
 public class FlappyBird extends JPanel implements ActionListener, KeyListener{
@@ -32,9 +35,14 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
     Font pixelFont;
     boolean gameOver = false;
     double score =0;
-    int selectedOption = 0; // lua chon man choi
+    int selectedOption = 0; // lua chon o menu
+    int selectedOption1 =0; // sau khi thua thi chon gi
+    int selectedOption2 = 0; // lua chon o high score
     int countdown =4;// dem count down
     boolean isCountingDown = false;
+    int highScore = 0;
+    boolean newHighScore = false;
+
     //so lương man
     enum GameState{
         MENU,
@@ -74,11 +82,40 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
             }
         });
         placePinesTimer.start();
+        //load highscore
+        loadHighScore();
         //game timer
         //60 khung hinh 1 giay
         gameLoop = new Timer(1000/60, this);
         gameLoop.start();
     }
+        // ham luu file
+        public void saveHighScore(){
+            try{
+                FileWriter writer = new FileWriter("highscore.txt");
+                writer.write(String.valueOf(highScore));
+                writer.close();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        //ham load file
+        public void loadHighScore(){
+            try{
+                File file = new File ("highscore.txt");
+                if(file.exists()){
+                    Scanner sc = new Scanner(file);
+                    if(sc.hasNextLine()){
+                        highScore = sc.nextInt();
+                    }
+                    sc.close();
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
 
     public void placePine(){
         int randomPipeY = (int)(0-pipeHeight/4-Math.random()*(pipeHeight/2));
@@ -104,8 +141,9 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
             drawPlaying(g);
         } else if (gameState==GameState.SELECT_BIRD) {
            // drawSelectBird(g);
-        } else if (gameState==GameState.HIGH_SCORE) {
             //drawHighScore(g);
+        } else if (gameState==GameState.HIGH_SCORE) {
+            drawHighScore(g);
         }
     }
     //Ve draw menu
@@ -116,7 +154,7 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
         String[] options = {
                 "Start Game",
                 "Select Bird",
-                "Hight Score"
+                "High Score"
         };
         int startX = 80;
         int startY = 250;
@@ -170,17 +208,82 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
         // score
         g.setColor(Color.white);
         g.setFont(pixelFont);
+//        //hien neu co ki luc moi
+//        if(newHighScore){
+//
+//        }
         if(gameOver){
+            //kiem tra xem co lap ki luc moi khong
+            if(highScore<score){
+                highScore = (int)score;
+                newHighScore = true;
+                saveHighScore();
+            }
+            //hien neu co ki luc moi
+            if (newHighScore) {
+                g.setFont(pixelFont.deriveFont(20f));
+                g.setColor(Color.YELLOW);
+
+                String text = "NEW HIGH SCORE!";
+                FontMetrics fm = g.getFontMetrics();
+                int textWidth = fm.stringWidth(text);
+
+                int x = (boardWidth - textWidth) / 2;
+
+                g.drawString(text, x, 80);
+            }
+            //hien thi sau khi thua game
             g.setColor(Color.BLACK);
             g.drawString("Game Over: "+String.valueOf((int)score),boardWidth/8+2,boardHeight/2+2);
             g.setColor(Color.WHITE);
             g.drawString("Game Over: "+String.valueOf((int)score),boardWidth/8,boardHeight/2);
+            int yOption =  boardHeight/2+40;
+            if(selectedOption1==0){
+                g.setColor(Color.black);
+            }else{
+                g.setColor(Color.yellow);
+            }
+            g.drawString("Again",boardWidth/2+5, yOption);
+            if(selectedOption1==1){
+                g.setColor(Color.black);
+            }else{
+                g.setColor(Color.yellow);
+            }
+            g.drawString("Menu", boardWidth/4-5, yOption);
         }
         else{
             g.setColor(Color.BLACK);
             g.drawString("Score: " + String.valueOf((int) score), 12, 37);
             g.setColor(Color.WHITE);
             g.drawString("Score: "+String.valueOf((int) score),10,35);
+        }
+    }
+
+    //ve highscore
+    public void drawHighScore(Graphics g){
+        g.drawImage(backgroudImg, 0, 0, boardWidth, boardHeight,null);
+        g.setFont(pixelFont.deriveFont(25F));
+        g.setColor(Color.white);
+        g.drawString("Flappy Bird",40,100);
+        g.setFont(pixelFont.deriveFont(20F));
+        g.drawString("HIGH SCORE",80, 220);
+        g.drawString(String.valueOf(highScore),140,250);
+        //nut bam
+        String[] option = {"PLAY", "MENU"};
+        int startX=100;
+        int startY=350;
+        int boxWidth=160;
+        int boxHeight=40;
+        for(int i=0;i<option.length;i++){
+            int y =startY + i*70;
+            if(i==selectedOption2){
+                g.setColor(Color.yellow);
+                g.fillRect(startX-10, y-30,boxWidth,boxHeight);
+                g.setColor(Color.black);
+            }else {
+                g.setColor(Color.white);
+            }
+            g.drawString(option[i],startX,y);
         }
     }
 
@@ -241,6 +344,7 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
         placePinesTimer.start();
         countdown =3;
         isCountingDown = true;
+        newHighScore = false;
     }
     //ham count down
     long lastTime = System.currentTimeMillis();
@@ -259,20 +363,12 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.VK_ESCAPE){
-            gameState=GameState.MENU;
-            repaint();
-        }
         if(gameState==GameState.PLAYING&&!isCountingDown){
             if(e.getKeyCode()==KeyEvent.VK_SPACE){
                 bird.jump();
-                if(gameOver){
-                    //reset game
-                    resetGame();
-                }
             }
         }
-        if(gameState==GameState.MENU){
+        else if(gameState==GameState.MENU){
             if(e.getKeyCode()==KeyEvent.VK_UP){
                 selectedOption--;
                 if(selectedOption<0) selectedOption=2;
@@ -293,6 +389,45 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
                     repaint();
                 } else if (selectedOption==2) {
                     gameState=GameState.HIGH_SCORE;
+                    repaint();
+                }
+            }
+            System.out.println(selectedOption);
+        }
+        // xu ly o man highscore
+        else if(gameState==GameState.HIGH_SCORE){
+            if(e.getKeyCode()==KeyEvent.VK_UP){
+                selectedOption2 = 0;
+                repaint();
+            } else if (e.getKeyCode()==KeyEvent.VK_DOWN) {
+                selectedOption2 = 1;
+                repaint();
+            } else if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+                if(selectedOption2==0){
+                    resetGame();
+                    gameState=GameState.PLAYING;
+
+                    handleCountDown();
+                    repaint();
+                }
+                if(selectedOption2==1){
+                    gameState=GameState.MENU;
+                    repaint();
+                }
+            }
+        }
+        if(gameOver){
+            if(e.getKeyCode()==KeyEvent.VK_LEFT){
+                selectedOption1 = 0;
+                repaint();
+            } else if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+                selectedOption1 = 1;
+                repaint();
+            } else if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+                if(selectedOption1==1) resetGame();
+                if(selectedOption1==0){
+                    gameState=GameState.MENU;
+                    gameOver=false;
                     repaint();
                 }
             }
